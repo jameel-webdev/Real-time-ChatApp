44ca4876a8b64219c8b4d912d5f881467a3270d4
Jameel Ahmed <jameel.webdev@gmail.com>
2024-02-07 12:29:11 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import { connectDb } from "./db/connectDb.js";
@@ -7,9 +8,10 @@ import userRoutes from "./routes/userRoutes.js";
 import cookieParser from "cookie-parser";
 import { app, server } from "./socket/socket.js";
 
-// const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(express.json()); // to parse the incoming request from JSON payloads (from req.body)
 app.use(express.urlencoded({ extended: true }));
@@ -19,9 +21,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => {
-  //root route http://localhost:5000/
-  res.send("Realtime ChatApp");
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 server.listen(PORT, () => {
